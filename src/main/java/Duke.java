847d1cc48e66620ddc972f2100d2a9a826d1c61f
LyuJiawen <gwendoline0812@gmail.com>
2020-08-24 21:40:11 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,31 +2,52 @@ import java.util.Scanner;
 
 public class Duke {
     
-    //print a welcome message
-    public static void welcomeMessage(){
+    //print the horizontal line
+    public static void horizontalLine(){
         String straightLine = "-------------------------------------------";
         System.out.println(straightLine);
+    }
+
+    //print a welcome message
+    public static void welcomeMessage(){
+        horizontalLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-        System.out.println(straightLine);   
+        horizontalLine();  
     }
     
     //print a goodbye message
     public static void byeMessage(){
-        String straightLine = "-------------------------------------------";
-        System.out.println(straightLine);
+        horizontalLine();
         System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(straightLine);
+        horizontalLine();
     }
     
     //echo the message
     public static void echoMessage(String message){
-        String straightLine = "-------------------------------------------";
-        System.out.println(straightLine);
-        System.out.println(message + "\n");
-        System.out.println(straightLine);
+        horizontalLine();
+        System.out.println("added: " + message + "\n");
+        horizontalLine();
     }
     
+    //echo the list
+    public static void echoList(String[] tasks){
+        horizontalLine();
+
+        int index = 0;
+        while (tasks[index] != null && index < tasks.length){
+            if (tasks[index].equals("list")){
+                break;
+            }
+            
+            System.out.println((index + 1) +". " + tasks[index]);
+            index++;
+        }
+
+        System.out.println();
+        horizontalLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -39,10 +60,20 @@ public class Duke {
         
         welcomeMessage();
         
-        String command = in.nextLine();
-        while (command != null && !command.equals("bye")) {
-            echoMessage(command);
-            command = in.nextLine();
+        String[] tasks = new String[100];
+        int index = 0;
+
+        tasks[index] = in.nextLine();
+        while (tasks[index] != null && !tasks[index].equals("bye")) {
+            if (tasks[index].equals("list")) {
+                echoList(tasks);
+                index--;
+            } else {
+                echoMessage(tasks[index]);
+            }          
+
+            index++;
+            tasks[index] = in.nextLine();
         }
         
         byeMessage();
